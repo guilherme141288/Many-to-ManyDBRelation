95dd90daeb9f6ab7bc01d66883206eacac8977f1
Guilherme Bertuol <82103102+guilherme141288@users.noreply.github.com>
2023-03-21 10:03:02 -0300

    Add files via upload

---
+++ roster1.py
@@ -0,0 +1,24 @@
+
+import json
+
+
+import json
+
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'roster_data_sample.json'
+
+# [
+#   [ "Charley", "si110", 1 ],
+#   [ "Mea", "si110", 0 ],
+
+str_data = open(fname).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+    
+    name = entry[0]
+    title = entry[1]
+    role = entry[2]
+
+    print((name, title, role))
